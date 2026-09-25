93995a99528abaeaa2314e75492a606de9bd6b62
ericrumsfeld <81602165+ericrumsfeld@users.noreply.github.com>
2026-09-25 00:37:45 +0000

    Deploying to gh-pages from @ ericrumsfeld/ericrumsfeld.github.io@9f7ba711ed90b9cc172d627de85b32f318047276 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,9 +62,9 @@ ninja.data = [{
           description: "Random Forest ML model in Python predicting 2026 NCAA Tournament outcomes",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project_MM26RFA/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-optimizing-geoguessr-plonks",
+          title: 'Optimizing Geoguessr Plonks',
+          description: "Ever wondered where exactly the highest EV Plonks are?",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
